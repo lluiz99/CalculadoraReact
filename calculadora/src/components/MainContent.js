81bd3eaa3aa19feaf301af3f5dcdaeb9520be757
lluiz99 <luiz.307.a@gmail.com>
2024-08-07 16:04:39 -0300

    React

---
+++ calculadora/src/components/MainContent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainContent() {
+    return (
+        <main>
+            <h2>Bem-vindo ao Meu Site</h2>
+            <p>Este é o conteúdo principal da página.</p>
+        </main>
+    )
+}
+export default MainContent;
